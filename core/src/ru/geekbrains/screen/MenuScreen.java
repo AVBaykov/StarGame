8de6d1ebfe5f4898c8d07f140b3c0e59402fb750
ABaykov <alexandr.baykov@gmail.com>
2019-03-14 14:21:29 +0300

    Added Background sprite, set world bounds to 100f

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -3,62 +3,50 @@ package ru.geekbrains.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.Base2DScreen;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.BadLogic;
 
 public class MenuScreen extends Base2DScreen {
-    private SpriteBatch batch;
+
+    private BadLogic badLogic;
+    private Background background;
     private Texture img;
-    private Vector2 touch;
-    private Vector2 v;
-    private Vector2 position;
-    private static int counter;
+    private Texture bndImg;
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
-        touch = new Vector2();
-        position = new Vector2(0, 0);
-        v = new Vector2(0.2f, 0.5f);
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
-        super.dispose();
+        bndImg = new Texture("background.jpg");
+        badLogic = new BadLogic(new TextureRegion(img));
+        background = new Background(new TextureRegion(bndImg));
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.51f, 0.34f, 0.64f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, position.x, position.y);
+        background.draw(batch);
+        badLogic.draw(batch);
         batch.end();
-        while (counter > 0) {
-            position.add(v);
-            counter--;
-            System.out.println(counter);
-        }
-    }
-
-    private void move(Vector2 position, Vector2 touch) {
-        Vector2 endPoint = touch.sub(position);
-        counter = (int) endPoint.len();
-        this.position = endPoint.nor();
+        badLogic.update();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        move(position, touch);
-        System.out.println("touch x =" + touch.x + " touch y = " + touch.y);
-        return super.touchDown(screenX, screenY, pointer, button);
+    public void dispose() {
+        img.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        badLogic.touchDown(touch, pointer);
+        return false;
     }
 }
