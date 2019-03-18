678ebfa3f779bdbf5653b55b2660ba7888d209e6
ABaykov <alexandr.baykov@gmail.com>
2019-03-18 14:59:53 +0300

    Lesson_04_HW

--- core/src/ru/geekbrains/sprite/Background.java
+++ core/src/ru/geekbrains/sprite/Background.java
@@ -3,10 +3,17 @@ package ru.geekbrains.sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import ru.geekbrains.base.Sprite;
+import ru.geekbrains.math.Rect;
 
 public class Background extends Sprite {
+
     public Background(TextureRegion region) {
         super(region);
-        setSize(100f,100f);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(worldBounds.getHeight());
+        pos.set(worldBounds.pos);
     }
 }
